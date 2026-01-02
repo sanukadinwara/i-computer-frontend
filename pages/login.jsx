32b124faea1f99392afcb54601c96a3e7cd18884
sanukadinwara <dinwarasanuka3@gmail.com>
2026-01-02 17:52:14 +0530

    Configuring how the front end displays the webpage that redirects users to based on the user's role, allowing or disallowing login based on the provided email and password. 🚀

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,13 +1,51 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage(){
+    const [email,setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const navigate = useNavigate()
+
+    function login(){
+
+        try{
+        
+        axios.post(import.meta.env.VITE_API_URL + "/users/login", 
+            {
+                email : email,
+                password : password
+            }
+        )
+        console.log(response)
+        toast.success("Login Successful")
+            if(response.data.role == "admin"){
+                navigate("/admin/")
+            }else{
+                navigate("/")
+            }
+        }catch(error){
+                toast.error("Login Failed")
+                console.log(error)
+            }
+    }
+
     return(
         <div className="w-full h-full bg-[url('/background.jpg')] bg-cover no-repeat flex justify-center items-center">
             <div className="absolute inset-0 bg-black opacity-60"></div>
                 <div className="relative backdrop-blur-md w-[450px] h-[450px] shadow-2xl rounded-2xl flex flex-col justify-center">
-                    <input type="email" placeholder="Email" className="m-5 p-3 w-[90%] rounded-lg border border-primary outline-none text-primary"/>
-                    <input type="password" placeholder="Password" className="m-5 p-3 w-[90%] rounded-lg border border-primary outline-none text-primary"/>
-                    <button className="m-5 p-3 w-[90%] h-[50px] rounded-lg bg-accent text-secondary font-bold">Login</button>
+                    <input type="email" placeholder="Email" onChange={
+                        (e)=>{
+                            setEmail(e.target.value)
+                        }
+                    } className="m-5 p-3 w-[90%] rounded-lg border border-primary outline-none text-primary"/>
+                    <input type="password" placeholder="Password" onChange={
+                        (e)=>{
+                            setPassword(e.target.value)
+                        }
+                    } className="m-5 p-3 w-[90%] rounded-lg border border-primary outline-none text-primary"/>
+                    <button onClick={login} className="m-5 p-3 w-[90%] h-[50px] rounded-lg bg-accent text-secondary font-bold">Login</button>
                     <p className="w-full text-center"><Link to="/password-reset" className="text-primary">Forget Password</Link></p>
                     
                     <div className="flex">
--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import AdminPage from "../pages/admin";
 import LoginPage from "../pages/login";
 import Test from "../pages/test";
 import { Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   
   return (
     <div className="w-full h-screen bg-red-600">
+      <Toaster position="top-center"/>
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/admin/*" element={<AdminPage/>} />
